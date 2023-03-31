b329fd63ff9f1dbd3929909eb9db1b82161d156d
hotwrist <odeyjhn@gmail.com>
2023-03-31 18:21:45 +0100

    Odeyemmy website first commit

---
+++ ux_libs/floating-whatsApp/float-whatsapp.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".float-whatsApp").floatingWhatsApp({
+        phone: "+2347069232807",
+        popupMessage: "Hello, how can I help you?",
+        showPopup: true,
+        headerTitle: "Welcome to Imperial Branding",
+        position: "right"
+    });
+
+})
